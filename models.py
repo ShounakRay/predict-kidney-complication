ac1ca9a35d0bf5b123bf65df970cd93e6a3cc53a
Shounak Ray <shounak@stanford.edu>
2022-11-26 11:35:42 -0800

    Data Viz, TODOs Modeling

--- models.py
+++ models.py
@@ -3,13 +3,16 @@
 # @Email:  shounak@stanford.edu
 # @Filename: models.py
 # @Last modified by:   shounak
-# @Last modified time: 2022-11-26T00:52:09-08:00
+# @Last modified time: 2022-11-26T11:30:29-08:00
 
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 import scipy
 from util import shuffle_data, nan_cols
+
+# TODO: Implement StandardScaler for normalization
+# TODO: Try out Sparse Regularization
 
 _ = """
 ################################################################################
@@ -27,6 +30,14 @@ ALL_DATA = pd.read_csv('Data/Merged Complete/Core_Dataset_SUPERVISED.csv').infer
 # ALL_DATA.drop('Patient Id', axis=1, inplace=True)
 TRAIN_SIZE = int(len(ALL_DATA) * TRAIN_PROPORTION)
 assert len(nan_cols(ALL_DATA)) == 0
+
+list(ALL_DATA)
+axarr = ALL_DATA.hist(bins=20, figsize=(100, 100))
+for ax in axarr.flatten():
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+plt.savefig('Images/Data_Histogram.png')
+
 
 # Create training and test data
 shuffled = shuffle_data(ALL_DATA)
